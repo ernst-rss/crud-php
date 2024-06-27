65da46a7ed9e1cd77d5737543317fa530404e6b3
ernst-rss <balisiernstjr@gmail.com>
2024-06-27 16:57:03 +0800

    Initialize Front End with no Functionality

---
+++ manage.php
@@ -0,0 +1,42 @@
+<?php
+    $username = "Placeholder";
+    $placeholder_id = "1";
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <input type="hidden" value="<?php echo $placeholder_id?>">    
+
+        <div class="container">
+            <div class="row m-3 p-3">
+                <label class="col-md-3">Username</label>
+                <input type="text" class="col-md-9" value="<?php echo $username ?>">
+            </div>
+
+            <div class="row m-3 p-3">
+                <label class="col-md-3">Password</label>
+                <input type="password" class="col-md-9">
+            </div>
+
+            <div class="row">  
+
+                <button type="button" class="btn btn-primary m-3">Terminate</button>
+                <button type="button" class="btn btn-primary m-3">Save</button>
+            </div>
+        </div>
+
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+    </body>
+</html>
